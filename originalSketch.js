902ff79d5b3fd39b65497566dc21287eec4d391d
Unknown <gzu200@nyu.edu>
2018-04-16 15:12:49 -0400

    put everything in same folder

---
+++ originalSketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+	tetrisShape();
+}
+
+function tetrisShape() {
+		for (var x = 0; x < width; x += 40){
+			for (var y = 0; y < height; y += 40){
+				fill(255-(x/10), 197-(y/2), y+3, x-3);
+				beginShape();
+					//vertex(x, y-10);
+					vertex(x+20, y-10);
+					vertex(x+20, mouseY-10);
+					vertex(x+40, y+10);
+					vertex(mouseX+20, y+30);
+					vertex(x, y+30);
+				endShape();
+			}
+		}
+}
